d9efad5be6a4aad9ccd52c3006efc49a15217e26
liul03 <liul03@WH-LIUL03.mysoft.com.cn>
2014-09-03 09:56:53 +0800

    作业

---
+++ pylearn001/py003.py
@@ -0,0 +1,28 @@
+#-*-coding:utf-8-*-
+print "I will now count my chickens:" #数有多少鸡
+
+print "Hens",25+30/6 #公鸡
+print "Roosters", 100-25*3%4 #母鸡
+
+print "Now I will count the eggs:" #数有多少蛋
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3+2<5-7?"
+
+print 3+2<5-7
+
+print "What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?",5>-2
+print "Is it greater or equal?",5>=-2
+print "Is it less or equal?",5<=-2
+
+print "what is 1*10*100*1000?", 1*10*100*1000
+
+print "what is the result?", -0.1-0.01+10.000
---
+++ pylearn001/py00406.py
@@ -0,0 +1,8 @@
+#-*-coding:utf-8-*-
+#变量i定义
+i = 8
+#j定义
+j = 4.0
+#计算i/j的值
+k =i/j
+print "k=", i/j
